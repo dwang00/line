c3ee3bfd4c5a17be9d34de8a7f2d3488bc86e011
David Wang <dwang00@stuy.edu>
2020-02-11 00:26:31 -0500

    asd

--- draw.c
+++ draw.c
@@ -8,20 +8,38 @@
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
   int a = y1 - y0;
-  int b = x1 - x0;
+  int b = -(x1 - x0);
   int d;
-  float m = a * 1.0 / b;
-  if (m >= 0 && m <= 1) {
+  float m = a * 1.0 / -b;
+  if (b > 0) {
+    draw_line(x1, y1, x0, y0, s, c);
+  }
+  //horizontal
+  if (a == 0) {
+    while(x0 <= x1) {
+      plot(s, c, x0, y0);
+      x0++;
+    }
+  }
+  //vertical
+  if (b == 0) {
+    while(y0 <= y1) {
+      plot(s, c, x0, y0);
+      y0++;
+    }
+  }
+  //1,5
+  if(0 <= m && m <= 1) {
     d = 2 * a + b;
-    while (x0 <= x1) {
+    while(x0 <= x1) {
       plot(s, c, x0, y0);
       if (d > 0) {
         y0++;
         d += 2 * b;
       }
       x0++;
+      d += 2 * a;
     }
-    d += 2 * a;
   }
   if (m > 1) {
     d = a + 2 * b;
@@ -32,7 +50,31 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
         d += 2 * a;
       }
       y0++;
+      d += 2 * b;
     }
-    d += 2 * b;
+  }
+  if (m < -1) {
+    d = a - 2 * b;
+    while (y0 >= y1) {
+      plot(s, c, x0, y0);
+      if (d < 0) {
+        x0++;
+        d -= 2 * a;
+      }
+      y0--;
+      d += 2 * b;
+    }
+  }
+  if (m >= -1 && m < 0) {
+    d = 2 * a - b;
+     while (x0 <= x1) {
+       plot(s, c, x0, y0);
+       if (d > 0) {
+         y0--;
+         d += 2 * b;
+       }
+       x0++;
+       d -= 2 * a;
+     }
   }
 }
